216d6d5456a0af615d5274cffcbabed8c452b295
Zach <zakkyzakkba@gmail.com>
2016-10-06 10:34:01 -0400

    Reformatted
    Alphabetical order

--- ConfigAPI/src/co/binarylife/config/util/ConfigurationLoader.java
+++ ConfigAPI/src/co/binarylife/config/util/ConfigurationLoader.java
@@ -66,57 +66,6 @@ public class ConfigurationLoader {
 		
 	}
 	
-	public static int toInteger(Object o){
-		return Integer.parseInt(o.toString());
-	}
-	
-	public static double toDouble(Object o){
-		return Double.parseDouble(o.toString());
-	}
-	
-	public static boolean toBoolean(Object o){
-		return Boolean.parseBoolean(o.toString());
-	}
-	
-	public static long toLong(Object o){
-		return Long.parseLong(o.toString());
-	}
-	
-	public static byte toByte(Object o){
-		return Byte.parseByte(o.toString());
-	}
-	
-	public static float toFloat(Object o){
-		return Float.parseFloat(o.toString());
-	}
-	
-	public static boolean isInt(Object o){
-		try{
-			Integer.parseInt(o.toString());
-			return true;
-		}catch(Exception e){
-			return false;
-		}
-	}
-	
-	public static boolean isDouble(Object o){
-		try{
-			Double.parseDouble(o.toString());
-			return true;
-		}catch(Exception e){
-			return false;
-		}
-	}
-	
-	public static boolean isLong(Object o){
-		try{
-			Long.parseLong(o.toString());
-			return true;
-		}catch(Exception e){
-			return false;
-		}
-	}
-	
 	public static boolean isBoolean(Object o){
 		try{
 			Boolean.parseBoolean(o.toString());
@@ -135,6 +84,15 @@ public class ConfigurationLoader {
 		}
 	}
 	
+	public static boolean isDouble(Object o){
+		try{
+			Double.parseDouble(o.toString());
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+	
 	public static boolean isFloat(Object o){
 		try{
 			Float.parseFloat(o.toString());
@@ -143,6 +101,43 @@ public class ConfigurationLoader {
 			return false;
 		}
 	}
-
+	
+	public static boolean isInt(Object o){
+		try{
+			Integer.parseInt(o.toString());
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+	
+	public static boolean isLong(Object o){
+		try{
+			Long.parseLong(o.toString());
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+	
+	public static boolean toBoolean(Object o){
+		return Boolean.parseBoolean(o.toString());
+	}
+	
+	public static byte toByte(Object o){
+		return Byte.parseByte(o.toString());
+	}
+	
+	public static double toDouble(Object o){
+		return Double.parseDouble(o.toString());
+	}
+	
+	public static int toInteger(Object o){
+		return Integer.parseInt(o.toString());
+	}
+	
+	public static long toLong(Object o){
+		return Long.parseLong(o.toString());
+	}
 	
 }
